151fa73511adb99d2a89f156b97c75585d8e7d6d
amyamyx <amyamyxtiffany@gmail.com>
2018-04-23 01:57:52 -0700

    Modify require_sequence

--- W2D1/skeleton/lib/simon.rb
+++ W2D1/skeleton/lib/simon.rb
@@ -7,6 +7,7 @@ class Simon
     @sequence_length = 1
     @game_over = false
     @seq = []
+    @input_seq = []
   end
 
   def play
@@ -21,19 +22,39 @@ class Simon
     require_sequence
     unless game_over
       round_success_message
-      add_random_color
       @sequence_length += 1
+      show_sequence
     end
 
   end
 
   def show_sequence
     add_random_color
+    @sequence_length.times do |i|
+      puts "Here's the sequence:"
+      puts @seq[i]
+      sleep(1)
+      system('clear')
+      sleep(0.5)
+    end
   end
 
   def require_sequence
-
+    puts "Enter a color sequence: "
+    puts @seq
+    input = gets.chomp
+    process_input(input)
+    @game_over = true unless @input_seq == @seq
   end
+
+  def process_input(input)
+    @input_seq = []
+    color = { "r" => "red", "b" => "blue", "g" => "green", "y" => "yellow"}
+    input.chars.each do |ch|
+      @input_seq << color[ch]
+    end
+  end
+
 
   def add_random_color
     @seq << COLORS.sample
@@ -51,5 +72,11 @@ class Simon
     @sequence_length = 1
     @game_over = false
     @seq = []
+    @input_seq = []
   end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  game = Simon.new
+  game.play
 end
