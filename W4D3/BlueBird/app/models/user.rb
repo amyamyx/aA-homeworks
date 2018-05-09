b45528d3aef0c223744a066333dfc0227d33e121
Tiffany Tang <amyamyxtiffany@gmail.com>
2018-05-09 02:45:42 -0700

    Modify ::find_by_credentials method

--- W4D3/BlueBird/app/models/user.rb
+++ W4D3/BlueBird/app/models/user.rb
@@ -20,7 +20,7 @@ class User < ApplicationRecord
 
   attr_reader :password
 
-  def self.find_by_credentials
+  def self.find_by_credentials(username, password)
     user = User.find_by(username: username)
     return user if user && BCrypt::Password.new(user.password_digest).is_password?(password)
     nil
